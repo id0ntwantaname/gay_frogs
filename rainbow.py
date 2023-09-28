6f8f47504ddee51a58f69410068ffa97475052ed
id0ntwantaname <120393307+id0ntwantaname@users.noreply.github.com>
2023-09-28 10:05:36 -0400

    Add files via upload

---
+++ rainbow.py
@@ -0,0 +1,38 @@
+#first type pip install bext in terminal 
+import time , sys
+import bext
+import os
+
+print("𝙂𝙖𝙮 𝙛𝙧𝙤𝙜𝙨")
+print("Press Ctrl + C to stop")
+
+time.sleep(2)
+
+indent = 0
+indentIncreasing = True
+
+while True:
+    print(" " * indent, end="")
+
+    bext.fg("red")
+    print("𓆏", end="")
+    bext.fg("yellow")
+    print("𓆏", end="")
+    bext.fg("green")
+    print("𓆏", end="")
+    bext.fg("blue")
+    print("𓆏", end="")
+    bext.fg("cyan")
+    print("𓆏", end="")
+    bext.fg("purple")
+    print("𓆏")
+
+    if indentIncreasing:
+        indent += 1
+    else:
+        indent -= 1
+    if indent == os.get_terminal_size()[0]-12:
+        indentIncreasing = False
+    if indent == 0:
+        indentIncreasing = True
+    time.sleep(0.0)
